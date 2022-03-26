4716c005dd3e76944a9cb75de926e78ee429cfbf
Kibria Islam <kibriaislam82@gmail.com>
2022-03-27 00:31:23 +0600

    select_related and prefetch_leated added

--- company/views.py
+++ company/views.py
@@ -6,9 +6,14 @@ from .models import Employee
 # Create your views here.
 
 def employee_data(request):
-
-    employees = Employee.objects.all()
+    #employees = Employee.objects.all().select_related("company")
+    employees = Employee.objects.all().prefetch_related("company")
 
     for employee in employees:
         print(employee.name,employee.company.name)
+    
+    data = {
+        'employees':employees
+    }
+    return render(request,'index.html', data)
     
